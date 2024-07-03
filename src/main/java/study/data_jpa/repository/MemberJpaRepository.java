44a0fc8706b722854790898d2f3e0946bc18cd12
BonSeung <bs.koo@sqisoft.com>
2024-07-03 15:11:18 +0900

    setting: spring initializing

---
+++ src/main/java/study/data_jpa/repository/MemberJpaRepository.java
@@ -0,0 +1,14 @@
+package study.data_jpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+}
